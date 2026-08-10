3d17f63c5a8405f3bcdd0b5afc827f37de3730ce
Coffee Espresso <coffeeespresso304@gmail.com>
2026-08-10 18:16:50 +0630

    feat: configure routing and app architecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Wishlist from "./pages/Wishlist";
+import ProductDetails from "./pages/ProductDetails";
+
 function App() {
-  return <h1 className="text-ink font-mono">Hello World</h1>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
